a297986779a12e99a34ed9958c50e1ad2b65ac69
Alex-Rein <darkozavr@gmail.com>
2024-07-11 22:42:01 +0500

    Pre migration models

--- bulletin_board/board/models.py
+++ bulletin_board/board/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    CATEGORIES = [
+        'Танки',
+        'Хилы',
+        'ДД',
+        'Торговцы',
+        'Гилдмастеры',
+        'Квестгиверы',
+        'Кузнецы',
+        'Кожевники',
+        'Зельевары',
+        'Мастера заклинаний',
+    ]
+
+    title = models.CharField(max_length=100, unique=True)
+    content = HTMLField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(choices=CATEGORIES)
+
+
+class Reply(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    post = models.OneToOneField(Post, on_delete=models.CASCADE)
+    text = models.TextField()
+    accept = models.BooleanField(default=False)
+
+
--- bulletin_board/bulletin_board/settings.py
+++ bulletin_board/bulletin_board/settings.py
@@ -9,23 +9,25 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(find_dotenv())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n8#t1*g2z+4ds4p2puau$rnw(us1e2b=+5f^_x7%l+a5+#x6tq'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'board',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'bulletin_board.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
